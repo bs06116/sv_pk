cb418746d92e1062761214c6ed219186fb7f1552
asim <iasimriaz@gmail.com>
2021-09-27 19:28:13 +0500

    add meta tags

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,6 +6,9 @@
     <title>Shinny View</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/jpg" href="{{asset('assets/img/webtop.jpeg')}}"/>
+    <meta name="description" content="ShinnyView: Pakistan’s Best Property Website. Now buy, rent and sell houses, plots, shops, flats easily in Pakistan."/>
+    <meta name="keywords" content="ShinnyView: Pakistan’s Best Property Website. Now buy, rent and sell houses, plots, shops, flats easily in Pakistan.">
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
